dca517eadcb31dd090dbee08a4a262423502da80
Jefferson Diefenbach <jefferson.diefenbach@uhk.com.br>
2020-08-30 16:24:59 -0300

    Remoção de dump de console

--- database/db.js
+++ database/db.js
@@ -4,7 +4,6 @@ let session = null;
 
 const connect = async () => {
   try {
-    console.log(process.env.MONGODB);
     await mongoose.connect(process.env.MONGODB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
