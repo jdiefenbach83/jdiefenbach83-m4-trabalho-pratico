910f036d3d1a99e0bc3b60d30a631cc9c7fee86b
Jefferson Diefenbach <jefferson.diefenbach@uhk.com.br>
2020-08-30 11:13:53 -0300

    Melhoria no processamento para mover contar para a agencia 'Private'

--- services/moveToPrivateService.js
+++ services/moveToPrivateService.js
@@ -9,42 +9,36 @@ const assembleMessage = (success, message) => {
 
 const move = async () => {
   try {
-    const customers = await accountModel.find(
-      {},
-      ['agencia', 'conta', 'name', 'balance'],
-      {
-        sort: { balance: -1 },
-      }
-    );
-
-    const accountsToMove = [];
-
-    for (let customer of customers) {
-      const exists = accountsToMove.some(
-        (item) => item.agencia === customer.agencia
-      );
-
-      if (exists) {
-        continue;
-      }
-
-      accountsToMove.push(customer);
-    }
+    const agencies = await accountModel.aggregate([
+      { $match: { agencia: { $ne: 99 } } },
+      { $group: { _id: { agency: '$agencia' } } },
+    ]);
 
     const movedAccounts = [];
     const updates = [];
 
-    accountsToMove.forEach(async ({ id, agencia, conta, name, balance }) => {
+    for (let agency of agencies) {
+      const customer = await accountModel.findOne(
+        { agencia: agency._id.agency },
+        ['agencia', 'conta', 'name', 'balance'],
+        {
+          sort: { balance: -1 },
+        }
+      );
+
+      const { id, agencia, conta, name, balance } = customer;
+
       updates.push({
         updateOne: { filter: { _id: id }, update: { agencia: 99 } },
       });
+
       movedAccounts.push({
         agencia,
         conta,
         name,
         balance,
       });
-    });
+    }
 
     const result = await accountModel.bulkWrite(updates);
     //fazer alguma validação junto com o transaction
